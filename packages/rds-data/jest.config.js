1630fdb56945c6f6942c53179c5099add70e0d76
Daniel Gördes <d.goerdes@overlap.at>
2022-05-05 14:20:01 +0200

    refactor(all): switch libs from SWC to TSC

--- packages/rds-data/jest.config.js
+++ packages/rds-data/jest.config.js
@@ -1,16 +1,13 @@
-const fs = require('fs');
-
-// Reading the SWC compilation config and remove the "exclude"
-// for the test files to be compiled by SWC
-const { exclude: _, ...swcJestConfig } = JSON.parse(
-    fs.readFileSync(`${__dirname}/.lib.swcrc`, 'utf-8'),
-);
-
 module.exports = {
     displayName: 'rds-data',
     preset: '../../jest.preset.js',
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+        },
+    },
     transform: {
-        '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
+        '^.+\\.[tj]s$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'js', 'html'],
     coverageDirectory: '../../coverage/packages/rds-data',
