87eac65b86fe3e46287df8b403a683716959ee04
Daniel Gördes <d.goerdes@overlap.at>
2022-07-07 12:02:55 +0200

    feat(api): adds logging for request auth context

--- packages/api/src/lib/logRequest.ts
+++ packages/api/src/lib/logRequest.ts
@@ -4,14 +4,44 @@ export const logRequest = (event: any) => {
     if (event.rawPath) {
         logHttpApiEvent(event);
     }
+
+    if (event.requestContext?.authorizer?.lambda) {
+        logLambdaAuthContext(event);
+    }
 };
 
 const logHttpApiEvent = (event: APIGatewayProxyEventV2) => {
-    console.log(
-        `REQUEST: ${event.rawPath}${
-            event.rawQueryString
-                ? '?' + decodeURIComponent(event.rawQueryString)
-                : ''
-        }`,
-    );
+    try {
+        console.log(
+            `REQUEST: ${event.rawPath}${
+                event.rawQueryString
+                    ? '?' + decodeURIComponent(event.rawQueryString)
+                    : ''
+            }`,
+        );
+    } catch (error) {
+        console.log('REQUEST: log failed');
+    }
+};
+
+const logLambdaAuthContext = (event: any) => {
+    try {
+        const authContext = event.requestContext?.authorizer?.lambda;
+        const loggableAuthContext = {
+            sub: authContext.sub,
+            org: authContext.org,
+            groups: authContext.groups,
+            policy: authContext.policy
+                ? JSON.parse(
+                      Buffer.from(authContext.policy, 'base64').toString(
+                          'ascii',
+                      ),
+                  )
+                : null,
+        };
+
+        console.log(`REQUEST AUTH: ${JSON.stringify(loggableAuthContext)}`);
+    } catch (error) {
+        console.log('REQUEST AUTH: log failed');
+    }
 };
