1875711ccb97b66aa7b47bace1cdc333352e53b8
Daniel Gördes <d.goerdes@overlap.at>
2022-09-07 10:31:28 +0200

    feat(api): adds ability to pass ajv options on middyfy and adds custom keyword "notEmpty"

--- packages/api/src/lib/middleware/middyfy.ts
+++ packages/api/src/lib/middleware/middyfy.ts
@@ -4,12 +4,19 @@ import cors from '@middy/http-cors';
 import middyJsonBodyParser from '@middy/http-json-body-parser';
 import httpSecurityHeaders from '@middy/http-security-headers';
 import validator from '@middy/validator';
+import Ajv, { Options as AjvOptions } from 'ajv';
 import type { Handler } from 'aws-lambda';
 import httpErrorHandler from './httpErrorHandlerMiddleware';
 
-type InputSchemaProps = {
+type GetInputSchemaProps = {
     bodySchema?: Record<string, any>;
     querySchema?: Record<string, any>;
+};
+
+type GetCompiledSchemaProps = {
+    inputSchema?: Record<string, any>;
+    outputSchema?: Record<string, any>;
+    ajvOptions?: AjvOptions;
 };
 
 type MiddyfyProps = {
@@ -17,13 +24,17 @@ type MiddyfyProps = {
     bodySchema?: Record<string, any>;
     querySchema?: Record<string, any>;
     outputSchema?: Record<string, any>;
+    ajvOptions?: AjvOptions;
 };
 
-export const ajvOptions = {
+const ajvDefaultOptions: AjvOptions = {
     strict: true,
     strictTypes: true,
     strictTuples: true,
     coerceTypes: false,
+    allErrors: true,
+    useDefaults: 'empty',
+    messages: false,
 
     // ! Do not change this !
     // This takes care to remove all additional properties
@@ -32,16 +43,13 @@ export const ajvOptions = {
     removeAdditional: true,
 };
 
-export const corsOptions = {
+const corsOptions = {
     origin: '*',
     headers: '*',
     methods: 'OPTIONS,POST,GET,PUT,PATCH,DELETE',
 };
 
-export const getInputSchema = ({
-    bodySchema,
-    querySchema,
-}: InputSchemaProps) => {
+const getInputSchema = ({ bodySchema, querySchema }: GetInputSchemaProps) => {
     // As the validator receives the whole event object.
     // We need to validate body and queries.
     let required: string[] = [];
@@ -81,19 +89,76 @@ export const getInputSchema = ({
     };
 };
 
+const getCompiledSchemas = ({
+    inputSchema,
+    outputSchema,
+    ajvOptions,
+}: GetCompiledSchemaProps) => {
+    let inputValidate = null;
+    let outputValidate = null;
+
+    const options = Object.assign({}, ajvDefaultOptions, ajvOptions);
+    const ajv = new Ajv(options);
+
+    ajv.addKeyword({
+        keyword: 'notEmpty',
+        errors: false,
+        validate: function (schema: boolean, data: any) {
+            if (schema === false) {
+                return true;
+            } else if (typeof data === 'string') {
+                return data.trim() !== '';
+            } else if (Array.isArray(data)) {
+                return data.length > 0;
+            } else if (typeof data === 'object') {
+                return Object.keys(data).length > 0;
+            } else {
+                return true;
+            }
+        },
+    });
+
+    if (inputSchema) {
+        inputValidate = ajv.compile(inputSchema);
+    }
+    if (outputSchema) {
+        outputValidate = ajv.compile(outputSchema);
+    }
+
+    return {
+        inputValidate,
+        outputValidate,
+    };
+};
+
 export const middyfy = ({
     handler,
     bodySchema,
     querySchema,
     outputSchema,
+    ajvOptions,
 }: MiddyfyProps) => {
-    const inputSchema = getInputSchema({ bodySchema, querySchema });
+    let inputSchema;
+    if (bodySchema || querySchema) {
+        inputSchema = getInputSchema({ bodySchema, querySchema });
+    }
 
-    if (bodySchema || querySchema || outputSchema) {
+    const { inputValidate, outputValidate } = getCompiledSchemas({
+        inputSchema,
+        outputSchema,
+        ajvOptions,
+    });
+
+    if (inputValidate || outputValidate) {
         return middy(handler)
             .use(doNotWaitForEmptyEventLoop())
             .use(middyJsonBodyParser())
-            .use(validator({ inputSchema, outputSchema, ajvOptions }))
+            .use(
+                validator({
+                    inputSchema: inputValidate,
+                    outputSchema: outputValidate,
+                }),
+            )
             .use(httpSecurityHeaders())
             .use(cors(corsOptions))
             .use(httpErrorHandler());
