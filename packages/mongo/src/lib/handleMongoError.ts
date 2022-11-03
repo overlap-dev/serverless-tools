0ecc3d005f29745bae13d45d2ae67cf9dbd4872b
Daniel Gördes <d.goerdes@overlap.at>
2022-11-03 16:26:18 +0100

    feat(mongo): extends mongodb error handler

--- packages/mongo/src/lib/handleMongoError.ts
+++ packages/mongo/src/lib/handleMongoError.ts
@@ -2,18 +2,25 @@ import { HttpError } from '@overlap/serverless-tools-api';
 import { MongoServerError } from 'mongodb';
 
 export const handleMongoError = (error: MongoServerError) => {
-    const badInputMessage = 'MongoDB Bad Input';
+    const badInputMessage = 'Bad Input: Database validation failed';
 
     if (error.code && error.code === 121) {
         throw new HttpError(badInputMessage, 400, {
             note: error.message,
             details: error.errInfo?.['details'],
         });
-    } else if (error.code && error.code === 2)
+    } else if (error.code && error.code === 2) {
         throw new HttpError(badInputMessage, 400, {
             note: error.message,
         });
-    else {
+    } else if (error.code && error.code === 11000) {
+        throw new HttpError(badInputMessage, 400, {
+            note: 'Duplicate key error',
+            details: {
+                keyValue: error['keyValue'],
+            },
+        });
+    } else {
         throw new HttpError('Internal Server Error', 500);
     }
 };
