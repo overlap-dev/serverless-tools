cf2d7365bc3292febf0b8bc29e474a462c47fdff
Daniel Gördes <d.goerdes@overlap.at>
2022-11-14 14:36:00 +0100

    feat(api): provide types for formatJSONResponse

--- packages/api/src/lib/formatJsonResponse.ts
+++ packages/api/src/lib/formatJsonResponse.ts
@@ -1,7 +1,22 @@
-export const formatJSONResponse = (
+export type FormatJSONResponseOutput = {
+    headers: {
+        [key: string]: string;
+        'Content-Type': 'application/json';
+    };
+    statusCode: number;
+    body: string;
+};
+
+export type FormatJSONResponse = <TResponse>(
     statusCode: number,
-    response: any | any[],
-    headers?: Record<string, unknown>,
+    response: TResponse,
+    headers?: Record<string, string>,
+) => FormatJSONResponseOutput;
+
+export const formatJSONResponse: FormatJSONResponse = (
+    statusCode,
+    response,
+    headers,
 ) => {
     return {
         headers: {
