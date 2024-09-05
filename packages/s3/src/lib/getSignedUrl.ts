9ebdb12eecce5dfc9a6476a913e60c1086502e74
Daniel Gördes <d.goerdes@overlap.at>
2024-09-05 11:16:46 +0200

    refactor(s3): remove type acrobatics

--- packages/s3/src/lib/getSignedUrl.ts
+++ packages/s3/src/lib/getSignedUrl.ts
@@ -1,21 +1,9 @@
-import { ServiceInputTypes } from '@aws-sdk/client-s3';
 import { getSignedUrl as awsGetSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { Command } from '@smithy/smithy-client';
-import { MetadataBearer, RequestPresigningArguments } from '@smithy/types';
+import { RequestPresigningArguments } from '@smithy/types';
 import { getS3Client } from './s3Client';
 
-export const getSignedUrl = <
-    InputTypesUnion extends object,
-    InputType extends InputTypesUnion,
-    OutputType extends MetadataBearer = MetadataBearer,
->(
-    command: Command<
-        InputType,
-        OutputType,
-        any,
-        ServiceInputTypes,
-        MetadataBearer
-    >,
+export const getSignedUrl = (
+    command: any,
     options?: RequestPresigningArguments,
 ) => {
     const s3Client = getS3Client();
